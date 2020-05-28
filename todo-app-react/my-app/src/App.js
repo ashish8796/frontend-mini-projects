16ba25845f1b45ca84326fa35dceebd9104fae57
ashish8796 <ashishsaini.ak@gmail.com>
2020-05-28 14:32:50 +0530

    Starting making todo react app

---
+++ todo-app-react/my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="heading">
+        <h1>Todos</h1>
+      </div>
+      <div className="todo-list"></div>
+      <form className="add-todo-form"></form>
+    </div>
+  );
+}
+
+export default App;
